3e615020b2a06179b530ffd0f08dda62031de345
SARAYUT1142 <sarayutinsopa764@gmail.com>
2026-05-14 17:39:29 +0700

    edite database.go

--- backend/database.go
+++ backend/database.go
@@ -1,23 +1,29 @@
-package main
-
-import (
-	"fmt"
-	"os"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
-var db *gorm.DB
-
 func InitDB() {
-	dsn := os.Getenv("database")
-	
-	var err error
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Printf("Database Error: %v\n", err)
-		os.Exit(1)
-	}
+    var dsn string
 
-	db.AutoMigrate(&Teacher{}, &Course{}, &Section{}, &Student{}, &Session{}, &Attendance{})
+    // ลองอ่านจาก env variable "database" ก่อน (ใช้ตอน local)
+    dsn = os.Getenv("database")
+
+    // ถ้าไม่มี → ประกอบจาก env variables แยก (ใช้ตอน Kubernetes)
+    if dsn == "" {
+        host     := os.Getenv("DB_HOST")
+        port     := os.Getenv("DB_PORT")
+        user     := os.Getenv("DB_USER")
+        password := os.Getenv("DB_PASSWORD")
+        dbname   := os.Getenv("DB_NAME")
+
+        dsn = fmt.Sprintf(
+            "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+            host, port, user, password, dbname,
+        )
+    }
+
+    var err error
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        fmt.Printf("Database Error: %v\n", err)
+        os.Exit(1)
+    }
+
+    db.AutoMigrate(&Teacher{}, &Course{}, &Section{}, &Student{}, &Session{}, &Attendance{})
 }
